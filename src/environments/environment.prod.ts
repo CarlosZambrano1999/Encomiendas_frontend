340c7212a7b5cc588dc7191f3f23c586f4bd455c
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2022-07-15 10:36:04 -0600

    version final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBkoRLZ5E-0D6-FZlLDrxoVjATRs0832LU",
+    authDomain: "pilarh-encomiendas.firebaseapp.com",
+    projectId: "pilarh-encomiendas",  
+    storageBucket: "pilarh-encomiendas.appspot.com",
+    messagingSenderId: "899176389044",
+    appId: "1:899176389044:web:b614e11ee487855fc0bae1",
+    measurementId: "G-8SCLTLGCTQ"
+  }
 };
